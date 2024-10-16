d75bd53d2b2be8250c87a70f8ad5f370d7a0bd03
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2024-10-16 14:40:25 +0900

    (14~16p)문제3 코드 간결화

--- 2주차/문제/(14~16p)문제3.py
+++ 2주차/문제/(14~16p)문제3.py
@@ -1,32 +1,12 @@
 import random
 
-n = int(input('선택하시오(1: 가위 2: 바위 3: 보)'))
+n = int(input('선택하시오(1: 가위 2: 바위 3: 보): '))
 b = random.randint(1, 3)
-print(f'컴퓨터의 선택(1: 가위 2: 바위 3: 보) {b}')
+print(f'컴퓨터의 선택(1: 가위 2: 바위 3: 보): {b}')
 
-if n == 1 and b == 1:
+if n == b:
     print('비겼음!')
-
-elif n == 1 and b == 2:
-    print('컴퓨터 승!')
-    
-elif n == 1 and b == 3:
+elif (n == 1 and b == 3) or (n == 2 and b == 1) or (n == 3 and b == 2):
     print('사용자 승!')
-
-elif n == 2 and b == 1:
-    print('사용자 승!')
-    
-elif n == 2 and b == 2:
-    print('비겼음!')
-
-elif n == 2 and b == 3:
-    print('컴퓨터 승!')
-    
-elif n == 3 and b == 1:
-    print('컴퓨터 승!')
-    
-elif n == 3 and b == 2:
-    print('사용자 승!')
-    
 else:
-    print('비겼음!')
+    print('컴퓨터 승!')
